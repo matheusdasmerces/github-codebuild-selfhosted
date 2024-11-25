e75fa305b109b509a69f0f26cf2d0adc1d715509
matheusdasmerces <matheusdasmerces@postnl.nl>
2024-11-25 15:08:59 +0100

    chore: add tests

--- lib/github-codebuild-selfhosted-stack.ts
+++ lib/github-codebuild-selfhosted-stack.ts
@@ -1,16 +1,37 @@
 import * as cdk from 'aws-cdk-lib';
+import { EventAction, FilterGroup, LinuxArmBuildImage, Project, ProjectVisibility, Source } from 'aws-cdk-lib/aws-codebuild';
+import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class GithubCodebuildSelfhostedStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'GithubCodebuildSelfhostedQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new Project(this, 'CodeBuildProject', {
+      projectName: 'github-codebuild-selfhosted',
+      source: Source.gitHub({
+        owner: 'matheusdasmerces',
+        repo: 'inversify-hexagonal-example',
+        webhook: true,
+        webhookFilters: [
+          FilterGroup.inEventOf(EventAction.WORKFLOW_JOB_QUEUED),
+        ],
+      }),
+      environment: {
+        buildImage: LinuxArmBuildImage.AMAZON_LINUX_2_STANDARD_3_0,
+        privileged: true,
+      },
+      logging: {
+        cloudWatch: {
+          logGroup: new LogGroup(this, 'CodeBuildLogGroup', {
+            logGroupName: '/aws/codebuild/github-codebuild-selfhosted',
+            retention: RetentionDays.ONE_WEEK,
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+          }),
+        },
+      },
+      queuedTimeout: cdk.Duration.minutes(20),
+      visibility: ProjectVisibility.PRIVATE,
+    });
   }
 }
--- test/github-codebuild-selfhosted.test.ts
+++ test/github-codebuild-selfhosted.test.ts
@@ -1,17 +1,15 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as GithubCodebuildSelfhosted from '../lib/github-codebuild-selfhosted-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as GithubCodebuildSelfhosted from '../lib/github-codebuild-selfhosted-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/github-codebuild-selfhosted-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new GithubCodebuildSelfhosted.GithubCodebuildSelfhostedStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('CodeBuild Project Created', () => {
+  const app = new cdk.App();
+    // WHEN
+  const stack = new GithubCodebuildSelfhosted.GithubCodebuildSelfhostedStack(app, 'MyTestStack');
+    // THEN
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  template.hasResourceProperties('AWS::CodeBuild::Project', {
+    Name: "github-codebuild-selfhosted",
+  });
 });
